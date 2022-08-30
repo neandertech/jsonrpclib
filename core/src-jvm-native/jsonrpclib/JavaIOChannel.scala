fa58b89db8e5594900daa7dc4b6a57f5ad7dc34d
Anton Sviridov <keynmol@gmail.com>
2022-08-30 09:07:12 +0100

    Semaphore stdout in Java implementation

--- core/src-jvm-native/jsonrpclib/JavaIOChannel.scala
+++ core/src-jvm-native/jsonrpclib/JavaIOChannel.scala
@@ -4,11 +4,9 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 import java.io.*
-import scala.concurrent.Promise
-import scala.annotation.tailrec
 
-class JavaIOChannel(in: DataInputStream, out: DataOutputStream, endpoints: List[Endpoint[Future]])(implicit
-    ec: ExecutionContext
+class JavaIOChannel(protected val in: DataInputStream, out: DataOutputStream, endpoints: List[Endpoint[Future]])(
+    implicit ec: ExecutionContext
 ) extends FutureBasedChannel(endpoints)
     with PlatformCompat {
 
@@ -17,37 +15,15 @@ class JavaIOChannel(in: DataInputStream, out: DataOutputStream, endpoints: List[
   }
 
   override def sendPayload(payload: Payload): Future[Unit] = {
-    Future {
+    Future.successful {
       import internals.*
       val head = LSPHeaders(payload.array.length, Constants.JSON_MIME_TYPE, "utf-8")
-      LSPHeaders.write(head, out)
-      out.write(payload.array)
-    }
-  }
 
-  final def start(): Future[Int] = {
-    val finish = Promise[Unit]()
-    try {
-      var fut = finish.future
-
-      @tailrec
-      def loop(): Unit = {
-        internals.LSPHeaders.readNext(in) match {
-          case Right(r) =>
-            val msg = new String(Array.fill(r.contentLength)(in.readByte))
-            executionContextLoop()
-            fut = handleReceivedPayload(Payload(msg.getBytes())).zip(fut).map(_._1)
-            loop()
-          case Left(_) =>
-            finish.completeWith(Future.unit)
-        }
+      PlatformCompat.exclusiveAccess {
+        LSPHeaders.write(head, out)
+        out.write(payload.array)
       }
-
-      loop()
-
-      fut.map(_ => 0)
-    } catch {
-      case _: EOFException => Future.successful(0)
     }
   }
+
 }
--- core/src-jvm/jsonrpclib/PlatformCompat.scala
+++ core/src-jvm/jsonrpclib/PlatformCompat.scala
@@ -1,5 +1,53 @@
 package jsonrpclib
 
-private[jsonrpclib] trait PlatformCompat {
+import scala.annotation.tailrec
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import java.io.EOFException
+import scala.concurrent.ExecutionContext
+import java.util.concurrent.Semaphore
+
+private[jsonrpclib] trait PlatformCompat { self: JavaIOChannel =>
+  final def start()(implicit ec: ExecutionContext): Future[Int] = {
+    val finish = Promise[Unit]()
+    try {
+      var fut = finish.future
+
+      @tailrec
+      def loop(): Unit = {
+        internals.LSPHeaders.readNext(in) match {
+          case Right(r) =>
+            val msg = new String(Array.fill(r.contentLength)(in.readByte))
+            PlatformCompat.executionContextLoop()
+            fut = handleReceivedPayload(Payload(msg.getBytes())).zip(fut).map(_._1)
+            loop()
+          case Left(_) =>
+            finish.completeWith(Future.unit)
+        }
+      }
+
+      loop()
+
+      fut.map(_ => 0)
+    } catch {
+      case _: EOFException => Future.successful(0)
+    }
+  }
+}
+
+private[jsonrpclib] object PlatformCompat {
   def executionContextLoop(): Unit = ()
+
+  private val semaphore = new Semaphore(1, true)
+
+  def exclusiveAccess[A](f: => A) = {
+
+    try {
+      semaphore.acquire()
+      f
+    } finally {
+      semaphore.release()
+    }
+
+  }
 }
