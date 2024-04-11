d3da1bb632394fe17ff6ecd4b11fce5ffbc3afc3
Anton Sviridov <keynmol@gmail.com>
2024-04-11 09:48:49 +0100

    Add a test for missing result field

--- core/test/src-jvm-native/jsonrpclib/RawMessageSpec.scala
+++ core/test/src-jvm-native/jsonrpclib/RawMessageSpec.scala
@@ -8,6 +8,8 @@ import jsonrpclib.OutputMessage.ResponseMessage
 
 class RawMessageSpec() extends FunSuite {
   test("json parsing with null result") {
+    // This is a perfectly valid response object, as result field has to be present,
+    // but can be null: https://www.jsonrpc.org/specification#response_object
     val rawMessage = readFromString[RawMessage](""" {"jsonrpc":"2.0","result":null,"id":3} """.trim)
     assertEquals(
       rawMessage,
@@ -15,5 +17,14 @@ class RawMessageSpec() extends FunSuite {
     )
 
     assertEquals(rawMessage.toMessage, Right(ResponseMessage(NumberId(3), Payload.NullPayload)))
+
+    // This, on the other hand, is an invalid response message, as result field is missing
+    val invalidRawMessage = readFromString[RawMessage](""" {"jsonrpc":"2.0","id":3} """.trim)
+    assertEquals(
+      invalidRawMessage,
+      RawMessage(jsonrpc = "2.0", result = None, id = Some(NumberId(3)))
+    )
+
+    assert(invalidRawMessage.toMessage.isLeft, invalidRawMessage.toMessage)
   }
 }
