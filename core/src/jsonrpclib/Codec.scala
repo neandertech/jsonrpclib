78ab76d4d29bb417cb8fa65bbd56d90bd6ff4a23
Anton Sviridov <velvetbaldmime@protonmail.com>
2024-04-11 10:15:04 +0100

    Fix parsing of responses with null result (#74)

--- core/src/jsonrpclib/Codec.scala
+++ core/src/jsonrpclib/Codec.scala
@@ -16,16 +16,21 @@ object Codec {
   def decode[A](payload: Option[Payload])(implicit codec: Codec[A]): Either[ProtocolError, A] = codec.decode(payload)
 
   implicit def fromJsonCodec[A](implicit jsonCodec: JsonValueCodec[A]): Codec[A] = new Codec[A] {
-    def encode(a: A): Payload = Payload(writeToArray(a))
+    def encode(a: A): Payload = {
+      Payload(writeToArray(a))
+    }
 
     def decode(payload: Option[Payload]): Either[ProtocolError, A] = {
       try {
         payload match {
-          case Some(Payload(array)) => Right(readFromArray(array))
-          case None                 => Left(ProtocolError.ParseError("Expected to decode a payload"))
+          case Some(Payload.Data(payload)) => Right(readFromArray(payload))
+          case Some(Payload.NullPayload)   => Right(readFromArray(nullArray))
+          case None                        => Left(ProtocolError.ParseError("Expected to decode a payload"))
         }
       } catch { case e: JsonReaderException => Left(ProtocolError.ParseError(e.getMessage())) }
     }
   }
+
+  private val nullArray = "null".getBytes()
 
 }
