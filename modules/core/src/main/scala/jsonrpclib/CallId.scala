a792e4d4dc34662fc0bcb65a580624d94a1c8583
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-05-10 22:31:43 +0200

    Rewrite CallIdDecoder with the use of or operator

--- modules/core/src/main/scala/jsonrpclib/CallId.scala
+++ modules/core/src/main/scala/jsonrpclib/CallId.scala
@@ -1,6 +1,6 @@
 package jsonrpclib
 
-import io.circe.{Decoder, Encoder, HCursor, Json}
+import io.circe.{Decoder, Encoder, Json}
 
 sealed trait CallId
 object CallId {
@@ -8,23 +8,17 @@ object CallId {
   final case class StringId(string: String) extends CallId
   case object NullId extends CallId
 
-  private[jsonrpclib] implicit val callIdDecoder: Decoder[CallId] = Decoder.instance { cursor =>
-    cursor.value.fold(
-      jsonNull = Right(NullId),
-      jsonNumber = num => num.toLong.map(NumberId(_)).toRight(decodingError(cursor)),
-      jsonString = str => Right(StringId(str)),
-      jsonBoolean = _ => Left(decodingError(cursor)),
-      jsonArray = _ => Left(decodingError(cursor)),
-      jsonObject = _ => Left(decodingError(cursor))
-    )
-  }
+  private[jsonrpclib] implicit val callIdDecoder: Decoder[CallId] =
+    Decoder
+      .decodeOption(Decoder.decodeString.map(StringId(_): CallId).or(Decoder.decodeLong.map(NumberId(_): CallId)))
+      .map {
+        case None    => NullId
+        case Some(v) => v
+      }
 
   private[jsonrpclib] implicit val callIdEncoder: Encoder[CallId] = Encoder.instance {
     case NumberId(n)   => Json.fromLong(n)
     case StringId(str) => Json.fromString(str)
     case NullId        => Json.Null
   }
-
-  private def decodingError(cursor: HCursor) =
-    io.circe.DecodingFailure("CallId must be number, string, or null", cursor.history)
 }
